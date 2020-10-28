be8ffa70d6e220099c1cd49b18374dfbd3f4ca71
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2020-10-29 01:09:51 +0530

    Corrected Reverse for home

--- authentication/views.py
+++ authentication/views.py
@@ -11,9 +11,9 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     elif request.user.is_authenticated:
-        return redirect('home')
+        return redirect('index')
     else:
         form = UserCreationForm()
         return render(request, 'signup.html', {'form': form})
