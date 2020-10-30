26cd066a941a732a8920938a5d0ad77183496239
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-10-31 01:15:04 +0530

    Updated Database Template and Modified Models

--- shop/models.py
+++ shop/models.py
@@ -1,27 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+from django.shortcuts import reverse
 
 class Seller(models.Model):
-    name = models.CharField(null=False, max_length=200)
-    contactNumber = models.IntegerField()
+    name = models.CharField(default="My Shop", null=False,
+                            max_length=30, blank=False)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, null=False)
+
+    class Meta:
+        verbose_name = ("Seller")
+        verbose_name_plural = ("Sellers")
+        ordering = ['name']
+        db_table = 'seller'
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ['name']
+    def get_absolute_url(self):
+        return reverse("Seller_detail", kwargs={"pk": self.pk})
 
 
 class Item(models.Model):
-    name = models.CharField(null=False, max_length=200)
-    price = models.IntegerField(null=False)
-    quantities_available = models.IntegerField(null=False)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    name = models.CharField(blank=False, null=False,
+                            max_length=40, primary_key=True, unique=True)
+    price = models.PositiveIntegerField(
+        blank=False, null=False, validators=[MinValueValidator(1)])
+    size = models.CharField(default='N.A.', null=False, max_length=20)
+    quantity_available = models.PositiveIntegerField(default=0, null=False)
+    min_quantity = models.PositiveIntegerField(default=0, null=False)
+    seller = models.ForeignKey(
+        Seller, on_delete=models.CASCADE, related_name="seller_items")
+
+    class Meta:
+        verbose_name = ("Item")
+        verbose_name_plural = ("Items")
+        ordering = ['name', 'price']
+        db_table = 'item'
 
     def __str__(self):
         return self.name
 
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True, null=False)
+
     class Meta:
-        ordering = ['name', 'price']
+        verbose_name = ("Notification")
+        verbose_name_plural = ("Notifications")
+        ordering = ['-date']
+        db_table = 'notification'
+
+    def __str__(self):
+        return self.name
 
 
 class BillItem(models.Model):
