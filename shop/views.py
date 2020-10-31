2d7db8e8c3f6cb95951d1bff6b3b27762ee68f18
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-10-31 17:32:09 +0530

    Removed Unnecesssary Imports

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'shop/index.html')
