25b6466b0b089728d9b93e4eb627c89445e4c37f
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-10-31 17:17:49 +0530

    Corrected links and templates

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('notifications', views.notifications, name='notifications'),
 ]
