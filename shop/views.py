3c2d920287a76e1219fab7cd2f7563265d3ffbbe
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-10-31 17:17:49 +0530

    Corrected links and templates

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'shop/index.html')
+
+def dashboard(request):
+    return render(request, 'shop/dashboard.html')
+    
+def notifications(request):
+    return render(request, 'shop/notifications.html')
