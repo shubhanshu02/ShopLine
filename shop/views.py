e695659d7515066bc1de0050bdc57de34ba6220a
Siddhant <siddhant.jain.cse19@itbhu.ac.in>
2020-11-02 01:11:29 +0530

    Added Items Available file and made Bill and Bill Item models.

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .models import Seller
+from .models import Item
 # Create your views here.
 def home(request):
     return render(request, 'shop/index.html')
 
 def dashboard(request):
-    return render(request, 'shop/dashboard.html')
+    items = Item.objects.order_by('name')
+    print(items)
+    return render(request, 'shop/Item_Available.html',{'items': items})
     
 def notifications(request):
     return render(request, 'shop/notifications.html')
+
+def Item_Available(request):
+    sellers = Seller.objects.filter(user = request.user)
+    print('sellers')
+    #for i in range(10):
+    #    nam = f'Item {i}'
+    #    Item(name = nam, price=  10, size= '500ml',quantity_available= 50,min_quantity=i+10, seller = sel).save()
+
+    return render(request, 'shop/Item_Available.html',{'posts': posts})
--- shopline/settings.py
+++ shopline/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'shopline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'itw',
+        'USER': 'username',
+        'PASSWORD': 'Password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 '''
@@ -95,6 +99,7 @@ DATABASES = {
         'PORT': '3306',
     }
 '''
+
 
 
 # Password validation
