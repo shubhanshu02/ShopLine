872a7bbd5f4155a6ffeeca5fdab91b934d7c2511
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-11-02 10:58:10 +0530

    Notifications Page and SQL Settings change

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from .models import Notification, Seller, Item
+import datetime
 # Create your views here.
+
+
 def home(request):
     return render(request, 'shop/index.html')
 
+
 def dashboard(request):
     return render(request, 'shop/dashboard.html')
-    
+
+
 def notifications(request):
-    return render(request, 'shop/notifications.html')
+    sel = Seller.objects.filter(user = request.user)[0]
+    for i in range(10):
+        nam = f'Item ${i}'
+        Item(name = nam, price=  10, size= '500ml',quantity_available= 50,min_quantity=i+10, seller = sel).save()
+
+
+    if request.user.is_authenticated:
+        notifications = Notification.objects.filter(user=request.user)
+        if (notifications.count() == 0):
+            return render(request, 'shop/notifications.html', {'message': "Nothing to Show"})
+        return render(request, 'shop/notifications.html', {'noti': notifications})
+    return render(request, 'shop/notifications.html', {'message': "You must be logged in to see this page"})
