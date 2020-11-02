e7bb6a58162c3499ea5d64229ad8a09c080a559d
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-11-02 11:12:46 +0530

    Deleted Invalid Address

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('notifications', views.notifications, name='notifications'),
-    path('Item_Available',views.Item_Available,name='Item_Available')
 ]
