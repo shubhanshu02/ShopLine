11474121cca217bd2dc7ec90818647aab2758609
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-11-02 11:48:23 +0530

    Corrected Fix

--- shop/views.py
+++ shop/views.py
@@ -12,16 +12,16 @@ def dashboard(request):
     if request.user.is_authenticated:
         seller = Seller.objects.filter(user=request.user)[0]
         item = Item.objects.filter(seller=seller)
-        if item.count() != 0:
+        if item.count() > 0:
             return render(request, 'shop/Item_Available.html', {'items': item})
-            return render(request, 'shop/Item_Available.html', {'message': "No Product to show"})
+        return render(request, 'shop/Item_Available.html', {'message': "No Product to show"})
     return render(request, 'shop/Item_Available.html', {'message': "Please Login to View this Page"})
 
 
 def notifications(request):
     if request.user.is_authenticated:
         notifications = Notification.objects.filter(user=request.user)
-        if (notifications.count() == 0):
-            return render(request, 'shop/notifications.html', {'message': "Nothing to Show"})
-        return render(request, 'shop/notifications.html', {'noti': notifications})
+        if (notifications.count() > 0):
+            return render(request, 'shop/notifications.html', {'noti': notifications})
+        return render(request, 'shop/notifications.html', {'message': "Nothing to Show"})
     return render(request, 'shop/notifications.html', {'message': "You must be logged in to see this page"})
