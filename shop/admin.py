76b47a44fb7fa9746dc12ffe7b2999245fe335a6
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-11-02 12:58:33 +0530

    Migration Changes and Seller Entry Creation on Signup (#12)
    * Fix #10
    
    * Corrected Fix
    
    * Create Seller Model on Signup
    
    * Create Seller Model on Signup

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Seller, Item, BillItem, Bill
+from .models import Seller, Item, BillItem, Bill, Notification
 
 admin.site.register(Seller)
 admin.site.register(Item)
 admin.site.register(BillItem)
 admin.site.register(Bill)
+admin.site.register(Notification)
